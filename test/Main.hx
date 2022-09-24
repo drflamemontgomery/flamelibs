310571a62b24cd9fa685fa685578540f112fcfa0
drflamemontgomery <drflamemontgomery@gmail.com>
2022-09-24 17:05:39 +1200

    Added test class

---
+++ test/Main.hx
@@ -0,0 +1,41 @@
+package;
+
+enum abstract TestState(Int) from Int to Int {
+    var NoState = 0;
+    var TestState1;
+    var TestState2;
+}
+
+class Main {
+
+    #if !macro
+    public static function main() {
+        dn.CiAssert.VERBOSE = true;
+        new Main();
+    }
+
+    public function new() {
+       testStateMachine();
+    }
+
+    function testState1() {
+        return TestState2;
+    }
+
+    function testState2() {
+        return TestState1;
+    }
+
+    function emptyStateFunc(s:TestState) {
+    }
+
+    function testStateMachine() {
+        var sm = new flm.StateMachine<TestState>(TestState1, NoState);
+        sm.addState(TestState1, testState1);
+        sm.addState(TestState2, testState2);
+        dn.CiAssert.equals(sm.state, TestState1);
+        sm.update();
+        dn.CiAssert.equals(sm.state, TestState2);
+    }
+    #end
+}
